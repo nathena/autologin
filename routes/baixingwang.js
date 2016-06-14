9f179fa767153d004cedb1d9e41f6bc0dd4848db
nathena@qq.com <nathena@qq.com>
2016-06-14 14:11:56 +0800

    anjuke login

---
+++ routes/baixingwang.js
@@ -0,0 +1,50 @@
+/**
+ * Created by nathena on 16/6/12.
+ */
+
+var uri = require("url");
+var util = require("util");
+var querystring = require("querystring")
+var request = require("request");
+var base = require("./base");
+
+var url = "http://user.anjuke.com/my/login";
+var loginApi = "http://member.anjuke.com/api/login/submit?%s&callback=window.user.callbackDetail";
+//username=3079340668%40qq.com&password=fangmm321&remember=true&captcha=
+
+module.exports = function(router){
+
+    router.get("/anjuke/:uid",function(req,res){
+        req.session["_uid"] = req.params["uid"];
+        res.render("anjuke.html",{baseurl:"/test",_csrf:req.csrfToken()})
+    })
+
+    router.post("/anjuke/:uid",function(req,res,next){
+
+        var reqheaders = base.getReqHeaders();
+
+        var uid = req.session["_uid"];
+        var data = {};
+        data["username"] = req.body["username"];
+        data["password"] = req.body["password"];
+        data["remember"] = "true";
+        data["captcha"] = "";
+
+        loginApi = util.format(loginApi,querystring.stringify(data));
+        console.log(" anjuke login url => "+url);
+
+        var set_cookies = {};
+        request.get({url:loginApi,headers:reqheaders},function(err,response,body){
+            if( err ){
+                return next(err);
+            }
+
+            set_cookies[url] = base.parserCookiejar(reqheaders,response.headers["set-cookie"]);
+            return base.storeCookie(uid,"s917",set_cookies,function(){
+                res.send(body);
+            });
+        })
+    })
+
+    return router;
+};
